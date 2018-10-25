972859f9f095542281cccfd40f31d27c973babeb
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-24 17:35:39 -0700

    Updated state values and UI design.

--- App.js
+++ App.js
@@ -9,58 +9,66 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       daysLeft: null,
-      weeksLeft: null,
-      monthsLeft: null,
-      daysInYear: null
+      daysInYear: null,
+      dayCompleted: null,
+      weekCompleted: null,
+      monthCompleted: null
     }
   }
 
   componentDidMount() {
     let currentTime = moment();
 
+    // How Much Year Left (via days left for better accuracy when displayed in progress bar)
     let endOfYear = moment().endOf('year');
     let daysLeft = endOfYear.diff(currentTime, 'days');
-    let weeksLeft = endOfYear.diff(currentTime, 'weeks');
-    let monthsLeft = endOfYear.diff(currentTime, 'months');
 
     let daysInYear = 0;
     for(let i = 1; i <= 12; i++) {
       daysInYear = daysInYear + moment(moment().format('YYYY') + '-' + i, 'YYYY-M').daysInMonth();
     }
 
+    // How Much Month Left
+    let daysInCurrentMonth = moment(moment().year() + '-' + (moment().month() + 1), 'YYYY-M').daysInMonth();
+    let monthCompleted = (moment().date())/daysInCurrentMonth;
+
+    // How Much Day Left
+    let dayCompleted = moment().hour() + 1;
+
     this.setState({
       daysLeft,
-      weeksLeft,
-      monthsLeft,
-      daysInYear
+      daysInYear,
+      dayCompleted,
+      monthCompleted
     });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style = {[styles.h1, styles.text]}>Oh No! The Year Is Ending!</Text>
-        <Text style = {styles.text}>Days Left: {this.state.daysLeft} days</Text>
-        <Text style = {styles.text}>Weeks Left: {this.state.weeksLeft} weeks</Text>
-        <Text style = {styles.text}>Months Left: {this.state.monthsLeft} months</Text>
+        <Text style = {[styles.h1, styles.text]}>The Year Is Ending!</Text>
+        <Text style = {[styles.label, styles.text]}>Day: </Text><Text style = {styles.text}>{24 - this.state.dayCompleted} Hours Left</Text>
+        <ProgressBarAndroid
+          styleAttr = "Horizontal"
+          indeterminate = {false}
+          progress = {this.state.dayCompleted/24}
+          color = "#e74c3c"
+          />
+
+        <Text style = {[styles.label, styles.text]}>Month: </Text><Text style = {styles.text}>{12 - (moment().month() + 1)} Months Left</Text>
+        <ProgressBarAndroid
+          styleAttr = "Horizontal"
+          indeterminate = {false}
+          progress = {this.state.monthCompleted}
+          color = "#1abc9c"
+          />
+
+        <Text style = {[styles.label, styles.text]}>Year: </Text><Text style = {styles.text}>{this.state.daysLeft} Days Left</Text>
         <ProgressBarAndroid
           styleAttr = "Horizontal"
           indeterminate = {false}
           progress = {(this.state.daysInYear - this.state.daysLeft)/this.state.daysInYear}
-          color = "#359dc7"
-        >
-        </ProgressBarAndroid>
-        <ProgressBarAndroid
-          styleAttr = "Horizontal"
-          indeterminate = {false}
-          progress = {this.state.weeksLeft}
-          color = "#359dc7"
-          />
-        <ProgressBarAndroid
-          styleAttr = "Horizontal"
-          indeterminate = {false}
-          progress = {(12-this.state.monthsLeft)/12}
-          color = "#359dc7"
+          color = "#3498db"
           />
       </View>
     );
@@ -72,12 +80,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    justifyContent: 'center'
   },
   h1: {
     fontSize: 24,
     fontWeight: '400'
+  },
+  label: {
+    fontWeight: '500'
   },
   text: {
     color: '#fff'
